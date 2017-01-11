602e27f8d6144926639363ed6e285d2e313db7b9
Angel Murchison <angelmurchison09@gmail.com>
2017-01-11 09:39:03 -0500

    Init commit.

---
+++ RestaurantDemo-Master/ResturantDemo/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResturantDemo.Models;
+
+namespace ResturantDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var menu = HttpContext.Cache["menu"] as IEnumerable<Category>;
+            if (menu == null)
+            {
+                menu = db.Categories.Include("Menu").OrderBy(o => o.Name).ToList();
+                HttpContext.Cache.Add(
+                    "menu",
+                    menu,
+                    null,
+                    System.Web.Caching.Cache.NoAbsoluteExpiration,
+                    new TimeSpan(0, 5, 0),
+                    System.Web.Caching.CacheItemPriority.Default,
+                    null
+                    );
+            }
+
+            return View(menu);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            HttpContext.Cache.Remove("menu");
+            db.Categories.Add(category);
+            db.SaveChanges();
+            var menu = db.Categories.Include("Menu").OrderBy(o => o.Name).ToList();
+            HttpContext.Cache.Add(
+                "menu",
+                menu,
+                null,
+                System.Web.Caching.Cache.NoAbsoluteExpiration,
+                new TimeSpan(0, 5, 0),
+                System.Web.Caching.CacheItemPriority.Default,
+                null
+                );
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = db.Categories.FirstOrDefault(f => f.Id == id);
+            return PartialView("_EditPartial", category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+
+        {
+            HttpContext.Cache.Remove("menu");
+            var toedit = db.Categories.FirstOrDefault(f => f.Id == category.Id);
+            toedit.Name = category.Name;
+            db.SaveChanges();
+            var menu = db.Categories.Include("Menu").OrderBy(o => o.Name).ToList();
+            HttpContext.Cache.Add(
+                "menu",
+                menu,
+                null,
+                System.Web.Caching.Cache.NoAbsoluteExpiration,
+                new TimeSpan(0, 5, 0),
+                System.Web.Caching.CacheItemPriority.Default,
+                null
+                );
+            return RedirectToAction("Index");
+        }
+
+    
+
+    public ActionResult Delete(int id)
+        {
+            var category = db.Categories.FirstOrDefault(f => f.Id == id);
+            return PartialView("Delete", category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            // var category = db.Categories.FirstOrDefault(f => f.Id == id);
+            HttpContext.Cache.Remove("menu");
+            var todelete = db.Categories.FirstOrDefault(f => f.Id == category.Id);
+            db.Categories.Remove(todelete);
+            db.SaveChanges();
+            var menu = db.Categories.Include("Menu").OrderBy(o => o.Name).ToList();
+            HttpContext.Cache.Add(
+                "menu",
+                menu,
+                null,
+                System.Web.Caching.Cache.NoAbsoluteExpiration,
+                new TimeSpan(0, 5, 0),
+                System.Web.Caching.CacheItemPriority.Default,
+                null
+                );
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ RestaurantDemo-Master/ResturantDemo/Controllers/MenuItemsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ResturantDemo.Models;
+
+namespace ResturantDemo.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MenuItems/Create
+        public ActionResult Create(int id)
+        {
+            var newmenuitem = new MenuItem()
+            {
+                CategoryId = id
+            };
+            return PartialView(newmenuitem);
+        }
+
+        // POST: MenuItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Description,Price,CategoryId")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MenuItems.Add(menuItem);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: MenuItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var menuitem = db.MenuItems.FirstOrDefault(f => f.Id == id);
+            return PartialView(menuitem);
+        }
+
+        // POST: MenuItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price,CategoryId")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menuItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var menuItem = db.MenuItems.FirstOrDefault(f => f.Id == id);
+            return PartialView(menuItem);
+        }
+
+        // POST: MenuItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MenuItem menuItem = db.MenuItems.Find(id);
+            db.MenuItems.Remove(menuItem);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
